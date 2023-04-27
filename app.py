53f33179720dc9135741b379cf38f8a1fb4cb58e
flintt <flintthuang@ymail.com>
2023-04-27 12:03:50 +0800

    将查询 IP 地址信息的逻辑封装成一个函数，以减少重复代码

--- app.py
+++ app.py
@@ -14,84 +14,50 @@ with open('country_asn.csv', 'r', encoding='utf-8') as f:
         row['end_ip'] = ipaddress.ip_address(row['end_ip'])
         data.append(row)
 
-# 查询IP地址所属国家或地区代码
+def find_ip_info(ip_address_str):
+    ip_address = ipaddress.ip_address(ip_address_str)
+    result = {}
+    for row in data:
+        if row['start_ip'].version == ip_address.version and row['start_ip'] <= ip_address <= row['end_ip']:
+            result['ip_address'] = str(ip_address)
+            result['country'] = row['country']
+            result['country_name'] = row['country_name']
+            result['continent'] = row['continent']
+            result['continent_name'] = row['continent_name']
+            result['asn'] = row['asn']
+            result['as_name'] = row['as_name']
+            result['as_domain'] = row['as_domain']
+            break
+    return result
+
 @app.route('/ip_country/<ip_address_str>')
 def ip_country(ip_address_str):
-    ip_address = ipaddress.ip_address(ip_address_str)
-    result = {}
-    for row in data:
-        if row['start_ip'].version == ip_address.version and row['start_ip'] <= ip_address <= row['end_ip']:
-            result['ip_address'] = str(ip_address)
-            result['country'] = row['country']
-            break
-    return json.dumps(result)
+    result = find_ip_info(ip_address_str)
+    return json.dumps({k: result[k] for k in result if k == 'ip_address' or k == 'country'})
 
-# 查询IP地址所属国家或地区名称
 @app.route('/ip_country_name/<ip_address_str>')
 def ip_country_name(ip_address_str):
-    ip_address = ipaddress.ip_address(ip_address_str)
-    result = {}
-    for row in data:
-        if row['start_ip'].version == ip_address.version and row['start_ip'] <= ip_address <= row['end_ip']:
-            result['ip_address'] = str(ip_address)
-            result['country_name'] = row['country_name']
-            break
-    return json.dumps(result)
+    result = find_ip_info(ip_address_str)
+    return json.dumps({k: result[k] for k in result if k == 'ip_address' or k == 'country_name'})
 
-# 查询IP地址所属洲代码
 @app.route('/ip_continent/<ip_address_str>')
 def ip_continent(ip_address_str):
-    ip_address = ipaddress.ip_address(ip_address_str)
-    result = {}
-    for row in data:
-        if row['start_ip'].version == ip_address.version and row['start_ip'] <= ip_address <= row['end_ip']:
-            result['ip_address'] = str(ip_address)
-            result['continent'] = row['continent']
-            break
-    return json.dumps(result)
+    result = find_ip_info(ip_address_str)
+    return json.dumps({k: result[k] for k in result if k == 'ip_address' or k == 'continent'})
 
-# 查询IP地址所属洲名称
 @app.route('/ip_continent_name/<ip_address_str>')
 def ip_continent_name(ip_address_str):
-    ip_address = ipaddress.ip_address(ip_address_str)
-    result = {}
-    for row in data:
-        if row['start_ip'].version == ip_address.version and row['start_ip'] <= ip_address <= row['end_ip']:
-            result['ip_address'] = str(ip_address)
-            result['continent_name'] = row['continent_name']
-            break
-    return json.dumps(result)
+    result = find_ip_info(ip_address_str)
+    return json.dumps({k: result[k] for k in result if k == 'ip_address' or k == 'continent_name'})
 
-# 查询IP地址所属自治系统（AS）信息
 @app.route('/ip_asn_info/<ip_address_str>')
 def ip_asn_info(ip_address_str):
-    ip_address = ipaddress.ip_address(ip_address_str)
-    result = {}
-    for row in data:
-        if row['start_ip'].version == ip_address.version and row['start_ip'] <= ip_address <= row['end_ip']:
-            result['ip_address'] = str(ip_address)
-            result['asn'] = row['asn']
-            result['as_name'] = row['as_name']
-            result['as_domain'] = row['as_domain']
-            break
-    return json.dumps(result)
+    result = find_ip_info(ip_address_str)
+    return json.dumps({k: result[k] for k in result if k == 'ip_address' or k == 'asn' or k == 'as_name' or k == 'as_domain'})
 
-# 查询IP地址完整信息
 @app.route('/ip_full_info/<ip_address_str>')
 def ip_full_info(ip_address_str):
-    ip_address = ipaddress.ip_address(ip_address_str)
-    result = {}
-    for row in data:
-        if row['start_ip'].version == ip_address.version and row['start_ip'] <= ip_address <= row['end_ip']:
-            result['ip_address'] = str(ip_address)
-            result['country'] = row['country']
-            result['country_name'] = row['country_name']
-            result['continent'] = row['continent']
-            result['continent_name'] = row['continent_name']
-            result['asn'] = row['asn']
-            result['as_name'] = row['as_name']
-            result['as_domain'] = row['as_domain']
-            break
+    result = find_ip_info(ip_address_str)
     return json.dumps(result)
 
 if __name__ == '__main__':
